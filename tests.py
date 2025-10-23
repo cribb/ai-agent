26f8189956727107ba3b73503eada03b578dafdd
Jeremy Cribb <cribb.jeremy@gmail.com>
2025-10-23 13:17:10 -0400

    Tweaked tests for get_file_contents

--- tests.py
+++ tests.py
@@ -1,12 +1,27 @@
 # python
 from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
 
-print("Running test #1")
-get_files_info("calculator", ".")
-print("Running test #2")
-get_files_info("calculator", "pkg")
-print("Running test #3")
-get_files_info("calculator", "/bin")
-print("Running test #4")
-get_files_info("calculator", "../")
+# print("Running tests for get_files_info...")
+# print("Test #1 (get_files_info)")
+# get_files_info("calculator", ".")
+# print("Test #2 (get_files_info)")
+# get_files_info("calculator", "pkg")
+# print("Test #3 (get_files_info)")
+# get_files_info("calculator", "/bin")
+# print("Test #4 (get_files_info)")
+# get_files_info("calculator", "../")
 
+print("\nRunning tests for get_file_content...")
+
+content = get_file_content("calculator", "main.py")
+print(content)
+
+content = get_file_content("calculator", "pkg/calculator.py")
+print(content)
+
+content = get_file_content("calculator", "/bin/cat")
+print(content)
+
+content = get_file_content("calculator", "pkg/does_not_exist.py")
+print(content)
