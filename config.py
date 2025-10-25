192e1e2f4af255a5e65ef9d39886916958200d8f
Jeremy Cribb <cribb.jeremy@gmail.com>
2025-10-25 11:59:48 -0400

    Now using a config file to house the constants used in the project.

---
+++ config.py
@@ -0,0 +1,20 @@
+# config file for ai-agent
+MODEL_NAME ='gemini-2.0-flash-001'
+AI_TIMEOUT = 30
+MAX_CHARS = 10000
+WORKING_DIRECTORY = "./calculator"
+MAX_AI_ITERATIONS = 30
+
+SYSTEM_PROMPT = """
+        You are a helpful AI coding agent.
+
+        When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+        - List files and directories
+        - Read file contents
+        - Execute Python files with optional arguments
+        - Write or overwrite files
+
+        All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+        """
+
