49167d55ee879294b7f86e8f895537d3e68a5de3
Jeremy Cribb <cribb.jeremy@gmail.com>
2025-10-26 07:47:00 -0400

    Updated system prompt.

--- config.py
+++ config.py
@@ -3,12 +3,13 @@ MODEL_NAME ='gemini-2.0-flash-001'
 AI_TIMEOUT = 30
 MAX_CHARS = 10000
 WORKING_DIRECTORY = "calculator"
-MAX_AI_ITERATIONS = 10
+MAX_AI_ITERATIONS = 30
 
 SYSTEM_PROMPT = """
         You are a helpful AI coding agent.
 
-        When a user asks a question or makes a request, make a function call plan. All paths you provide should be relative to the working directory. 
+        When a user asks a question or makes a request, make a function call plan. 
+        All paths you provide will be relative to the working directory. 
         You do not need to specify the working directory in your function calls as it is automatically injected for security reasons. 
         You can perform the following operations within the working directory AND its subdirectories. 
 
@@ -17,8 +18,11 @@ SYSTEM_PROMPT = """
         - Execute Python files with optional arguments
         - Write or overwrite files
         
-        If you cannot find the information you need, do not forget to search deeper inside the directory tree. Ensure that you pursue your operations across the directory tree for which you have access. 
+        If you cannot find the information you need, do not forget to search deeper inside your working directory's tree.
+        Do not fail to check the pkg subdirectory.
+        Ensure that you pursue your operations across the directory hierarchy/tree for which you DO have access. 
         Unless new info was added to messages, avoid calling the same tool with identical parameters twice in a row, for that way lies insanity. Also, don't quit before trying other options.
-        Run whatever code and write whatever data is necessary to answer the user's question. When fixing bugs in the calculator project (for which you DO have access), focus on the code files that exist. Don't cheat to get the correct answer.
+        Run whatever code and write whatever data is necessary to answer the user's question. When fixing bugs in the calculator project (for which you DO have access), focus on the code files that exist. Don't cheat to get the correct answer. 
+        For bug-fixing, you already have access to a series of unit tests to check your changes.
         """
 
